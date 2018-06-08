9c436e426643c48a0ac27f8a5c9832926606f256
Andrew Xu <alexandrew94@hotmail.com>
2018-06-08 11:27:12 +0100

    added short bio animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+document.addEventListener('scroll', titleScroll);
+document.addEventListener('scroll', bioScroll);
+
+function titleScroll() {
+  const windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  const distanceFromTop = Math.abs(document.getElementsByTagName('header')[0].getBoundingClientRect().top);
+  const distanceToScroll = (windowHeight - distanceFromTop) / windowHeight;
+  document.getElementsByClassName('logo')[0].style.top = `${ (1 - (distanceToScroll / 2)) * 100}%`;
+  document.getElementsByClassName('logo')[0].style.opacity = ((distanceToScroll * 2) - 0.5);
+}
+
+function bioScroll() {
+  const windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  const distanceFromTop = document.getElementsByClassName('short-bio')[0].getBoundingClientRect().top;
+  console.log(distanceFromTop);
+  const distanceToScroll = (windowHeight - distanceFromTop) / windowHeight;
+  document.getElementsByClassName('short-bio')[0].style.top = `${ (1 - (distanceToScroll / 2)) * 100 - 50 }%`;
+  document.getElementsByClassName('short-bio')[0].style.opacity = ((distanceToScroll * 2) - 0.05);
+}
+
 let counter = 1;
 setInterval(() => {
   const displayJobs = [
