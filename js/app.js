9ef4b20c37eb32b03e1819895b2e448328448e40
Andrew Xu <alexandrew94@hotmail.com>
2018-06-12 15:59:29 +0100

    made changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
-document.addEventListener('scroll', titleScroll);
-document.addEventListener('scroll', bioScroll);
+document.addEventListener('scroll', () => {
+  titleScroll();
+  bioScroll();
+  unHideAll();
+  fadeUpAndIn();
+});
+
+function fadeUpAndIn() {
+  [...document.getElementsByClassName('fade-up-and-in')].forEach(element => {
+    element.currentlyInView = false;
+    if (element.getBoundingClientRect().top <= window.innerHeight && !element.currentlyInView) {
+      element.classList.add('fade-up-and-in-animation');
+      element.currentlyInView = true;
+    } else if (element.getBoundingClientRect().top > window.innerHeight + 100 && !element.currentlyInView) {
+      element.classList.remove('fade-up-and-in-animation');
+      element.currentlyInView = true;
+    }
+  });
+}
+
+function unHideAll() {
+  [...document.getElementsByTagName('section')].forEach(element => {
+    element.removeAttribute('hidden');
+  });
+}
 
 function titleScroll() {
   const windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
@@ -11,10 +34,10 @@ function titleScroll() {
 
 function bioScroll() {
   const windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-  const distanceFromTop = document.getElementsByClassName('short-bio')[0].getBoundingClientRect().top;
+  const distanceFromTop = document.getElementsByClassName('short-bio-box')[0].getBoundingClientRect().top;
   const distanceToScroll = (windowHeight - distanceFromTop) / windowHeight;
-  document.getElementsByClassName('short-bio')[0].style.top = `${ (1 - (distanceToScroll / 2)) * 100 - 50 }%`;
-  document.getElementsByClassName('short-bio')[0].style.opacity = ((distanceToScroll * 2) - 0.05);
+  document.getElementsByClassName('short-bio-box')[0].style.top = `${ (1 - (distanceToScroll / 4)) * 100 - 50 }%`;
+  document.getElementsByClassName('short-bio-box')[0].style.opacity = ((distanceToScroll * 2) - 0.05);
 }
 
 let counter = 1;
