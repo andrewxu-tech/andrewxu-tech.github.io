5e11970c773b0921316968abb872d4c0d9db15d8
Andrew Xu <alexandrew94@hotmail.com>
2018-06-15 22:00:01 +0100

    added smooth scrolling

--- js/app.js
+++ js/app.js
@@ -18,7 +18,8 @@ setInterval(() => {
     '<h1><span class="type-of-developer">JavaScript</span><br />developer</h1>',
     '<h1><span class="type-of-developer">full-stack</span><br />developer</h1>',
     '<h1><span class="type-of-developer">ReactJS</span><br />developer</h1>',
-    '<h1><span class="type-of-developer">Node.js</span><br /><span class="fade-after-4-secs">developer</span></h1>',
+    '<h1><span class="type-of-developer">Node.js</span><br />developer</h1>',
+    '<h1><span class="type-of-developer">HTML<br />& CSS</span><br /><span class="fade-after-4-secs">developer</span></h1>',
     '<h1><span class="type-of-developer">composer,</span><br /><span class="type-of-developer">violinist</span></h1>'
   ];
   document.getElementsByClassName('job')[0].innerHTML = displayJobs[counter % displayJobs.length];
@@ -51,6 +52,15 @@ function unHideAll() {
       element.setAttribute('hidden', true);
     });
     document.getElementsByClassName(element.classList[1])[1].removeAttribute('hidden');
+    window.smoothScrollTo(0, 1000, 1500);
+  });
+});
+
+[...document.getElementsByClassName('content-nav-arrow')].forEach(element => {
+  element.addEventListener('click', () => {
+    const scrollToElementClass = element.classList[1];
+    const scrollToElementLocation = offset(document.getElementsByClassName(scrollToElementClass)[1]).top - window.innerHeight / 15;
+    window.smoothScrollTo(0, scrollToElementLocation, 1200);
   });
 });
 
@@ -61,3 +71,35 @@ function titleScroll() {
   document.getElementsByClassName('logo')[0].style.top = `${(1 - (distanceToScroll / 2)) * 100}%`;
   document.getElementsByClassName('logo')[0].style.opacity = ((distanceToScroll * 2) - 0.5);
 }
+
+function offset(el) {
+  var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+}
+
+window.smoothScrollTo = function(endX, endY, duration) {
+  var startX = window.scrollX || window.pageXOffset,
+    startY = window.scrollY || window.pageYOffset,
+    distanceX = endX - startX,
+    distanceY = endY - startY,
+    startTime = new Date().getTime();
+
+  duration = typeof duration !== 'undefined' ? duration : 400;
+
+  var easeInOutQuart = function(time, from, distance, duration) {
+    if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+    return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  var timer = window.setInterval(function() {
+    var time = new Date().getTime() - startTime,
+      newX = easeInOutQuart(time, startX, distanceX, duration),
+      newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      window.clearInterval(timer);
+    }
+    window.scrollTo(newX, newY);
+  }, 1000 / 60);
+};
